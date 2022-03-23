0e12108c52b3df9921b2f662ab466ae8267a7b34
mbans <mbans>
2022-03-23 00:12:01 +0000

    Initial commit

---
+++ src/test/java/com/lumar/broker/BrokerTest.java
@@ -0,0 +1,21 @@
+package com.lumar.broker;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class BrokerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
